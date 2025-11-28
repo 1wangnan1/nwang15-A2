bea8cc730748b59b4506875fb3e0b684fb821b04
wangnan <318574212@qq.com>
2025-11-28 21:35:19 +0800

    add Ride Interface

---
+++ src/RideInterface.java
@@ -0,0 +1,26 @@
+// RideInterface.java
+public interface RideInterface {
+    // add visitor to queue
+    void addVisitorToQueue(Visitor visitor);
+    
+    // remove visitor from queue
+    void removeVisitorFromQueue();
+    
+    // print waiting visitors
+    void printQueue();
+    
+    // add visitor to ride history
+    void addVisitorToHistory(Visitor visitor);
+    
+    // check if visitor in history
+    boolean checkVisitorFromHistory(Visitor visitor);
+    
+    // get number of visitors in history
+    int numberOfVisitors();
+    
+    // print ride history
+    void printRideHistory();
+    
+    // run one cycle of ride
+    void runOneCycle();
+}
